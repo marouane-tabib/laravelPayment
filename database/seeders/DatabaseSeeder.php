920ba5ddd4fb96c5dfd38bc29b6d5b368be1a0f0
marwan-tabib <marwanmaroc297@gmail.com>
2022-10-31 15:34:37 +0100

    4. Preparing the payment platform and filling the database from Laravel

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $this->call(PaymentPlatformsTableSeeder::class);
+        $this->call(CurrenciesTableSeeder::class);
     }
 }
---
+++ database/seeders/PaymentPlatformsTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\PaymentPlatform;
+
+class PaymentPlatformsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        PaymentPlatform::create([
+            'name' => 'Paypal',
+            'image' => 'img/payment-platform/paypal.jpg',
+        ]);
+        PaymentPlatform::create([
+            'name' => 'Stripe',
+            'image' => 'img/payment-platform/stripe.jpg',
+        ]);
+    }
+}
