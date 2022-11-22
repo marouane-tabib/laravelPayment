04c9b54240a2e1ba931652d762dc07cdaa908ccc
marwan-tabib <marwanmaroc297@gmail.com>
2022-11-22 20:19:20 +0100

    2. Considering zero-decimal currencies

--- app/Services/PayPalService.php
+++ app/Services/PayPalService.php
@@ -75,7 +75,7 @@ class PayPalService{
                     0 => [
                         'amount' => [
                             'currency_code' => strtoupper($currency),
-                            'value' => $value
+                            'value' => round($value * $factor = $this->resolveFactor($currency)) / $factor,
                         ]
                     ]
                 ],
@@ -102,5 +102,12 @@ class PayPalService{
                 'Content-Type' => 'application/json'
             ],
         );
+    }
+    public function resolveFactor($currency){
+        $zeroDecimalCurrencies = ['JPY'];
+        if(in_array(strtoupper($currency) , $zeroDecimalCurrencies)){
+            return 1;
+        }
+        return 100;
     }
 }
