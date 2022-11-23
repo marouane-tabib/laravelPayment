7b84d5d24e99f89118bcdb9273e536843a93be29
marwan-tabib <marwanmaroc297@gmail.com>
2022-11-23 20:56:13 +0100

    3. Creating a component that solves a payment platform dynamically

---
+++ app/Http/Resolvers/PaymentPlatformResolver.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Resolvers;
+
+use App\Models\PaymentPlatform;
+
+class PaymentPlatformResolver{
+
+    protected $paymentPlatform;
+
+    public function __construct()
+    {
+        $this->paymentPlatform = PaymentPlatform::all();
+    }
+
+    public function resolveService($paymentPlatformId){
+        $name = strtolower($this->paymentPlatform->firstWhere('id' , $paymentPlatformId)->name);
+
+        $service = config("services.{$name}.class");
+        if($service){
+            return resolve($service);
+        }
+
+        throw new \Exception('The selected platform is not in the configuration');
+    }
+}
