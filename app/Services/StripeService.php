e7afe17ca90dc8544b087ffe5d8283853acd47da
marwan-tabib <marwanmaroc297@gmail.com>
2022-12-16 19:45:46 +0100

    3. Handling with Laravel the creation of a payment intent in Stripe

--- app/Services/StripeService.php
+++ app/Services/StripeService.php
@@ -34,7 +34,12 @@ class StripeService{
     }
 
     public function handlePayment(Request $request){
-        //
+        $request->validate([
+            'payment_method' => 'required',
+        ]);
+        $intent = $this->createIntent($request->value,$request->currency,$request->payment_method);
+        session()->put('paymentIntentId' , $intent->id);
+        return redirect()->route('approval');
     }
 
     public function handleApproval(){
