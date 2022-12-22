d6b705cc049877b08f966d8ebf15dbc85a641072
marwan-tabib <marwanmaroc297@gmail.com>
2022-12-22 21:42:05 +0100

    3. Performing 3D Secure validation directly with Stripe

--- resources/views/stripe/3d-secure.blade.php
+++ resources/views/stripe/3d-secure.blade.php
@@ -41,5 +41,17 @@
                 </div>
             </div>
         </div>
+            <script src="https://js.stripe.com/v3/"></script>
+            <script>
+                const stripe = Stripe('{{ config('services.stripe.key') }}');
+                stripe.handleCardAction("{{ $clientSecret }}").then(function(result){
+                    if(result.error){
+                        window.location.replace("{{ route('cancelled') }}");
+                    }else{
+                        window.location.replace("{{ route('approval') }}");
+                    }
+                })
+            </script>
+
     </body>
 </html>
