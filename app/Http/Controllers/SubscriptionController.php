808e0c83ed9607c78628a65c4c7b29ffd887e110
marwan-tabib <marwanmaroc297@gmail.com>
2023-02-10 17:25:18 +0100

    5. Start the Payment of a Subscription With a Payment Platform

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -5,26 +5,38 @@ namespace App\Http\Controllers;
 use App\Models\PaymentPlatform;
 use App\Models\Plan;
 use App\Models\Subscription;
+use App\Resolvers\PaymentPlatformResolver;
 use Illuminate\Http\Request;
 
 class SubscriptionController extends Controller
 {
-    public function __construct()
+    protected $paymentPlatformResolver;
+
+    public function __construct(PaymentPlatformResolver $paymentPlatformResolver)
     {
         $this->middleware('auth');
+        $this->paymentPlatformResolver = $paymentPlatformResolver;
     }
     public function show(){
         $paymentPlatforms = PaymentPlatform::
-            // where('subscriptions_enabled' , true)->
-            get();
+            where('subscriptions_enabled' , true)
+            ->get();
         return view('subscribe')->with([
             'plans' => Plan::all(),
             'paymentPlatforms' => $paymentPlatforms,
         ]);
     }
 
-    public function store(){
-        //
+    public function store(Request $request){
+        $rules = [
+            'plan' => ['required' , 'exists:plans,slug'],
+            'payment_platform' => ['required' , 'exists:payment_platforms,id'],
+        ];
+        $request->validate($rules);
+
+        $paymentPlatform = $this->paymentPlatformResolver->resolveService($request->payment_platform);
+        session()->put('subscriptionPlatformId' , $request->payment_platform);
+        return $paymentPlatform->handleSubscription($request);
     }
 
     public function approval(){
---
+++ q
@@ -0,0 +1,11 @@
+  10_Adding_the_essential_elements_in_Laravel_to_start_charging_with_Stripe[m
+  12_Handling_Strong_Customer_Authentication_SCA_with_Stripe_and_Laravel[m
+* [32m13_Preparing_a_Subscription_System_for_the_Project[m
+  3_preparing_some_visual_components_in_laravel_to_process_payments[m
+  5_Allowing_to_consume_the_API_of_any_payment_platform_from_laravel[m
+  6_preparing_laravel_to_use_PayPal_as_a_payment_platform[m
+  7_implementing_the_necessary_actions_in_laravel_to_use_the_PayPal_api[m
+  8_Improving_and_preparing_Laravel_to_use_any_other_payment_platform[m
+  implementing_the_necessary_actions_in_laravel_to_use_the_PayPal_api[m
+  main[m
+  remove[m
